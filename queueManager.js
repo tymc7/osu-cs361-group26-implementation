4b83263411f5f314914d1908ee0207dfde56a053
Ryan Shin <shinr@flip1.engr.oregonstate.edu>
2017-11-16 22:12:24 -0800

    WIP

--- queueManager.js
+++ queueManager.js
@@ -43,7 +43,7 @@ function patientNode ( pid, time, priority) {
     this.priority = priority;
 }
 
-// Test for pid's matching
+// test for pid's matching
 // Time should never match, but if they do, system shouldnt fail
 
 var p1 = new patientNode(2, 5, 2);
@@ -54,24 +54,27 @@ var p3 = new patientNode(3, 9, 2);
 
 var queue = new priorityQueue();
 
-queue.pushpatient(p1);
+queue.pushPatient(p1);
 queue.pushPatient(p2);
 queue.pushPatient(p3);
 
 //p2 > p1 > p3
-assert.deepEqual(queue.peekPatient(), p2);
-queue.popPatient();
-assert.deepEqual(queue.peekPatient(), p1);
-queue.popPatient();
-assert.deepEqual(queue.peekPatient(), p3);
-
-
-//p1 > p2 > p3
-//queue.prioritize(p1, 100);
-//
-//assert.deepEqual(queue.peekPatient(), p1);
-//queue.popPatient();
 //assert.deepEqual(queue.peekPatient(), p2);
+//queue.popPatient();
+//assert.deepEqual(queue.peekPatient(), p1);
 //queue.popPatient();
 //assert.deepEqual(queue.peekPatient(), p3);
 
+
+//p1 > p2 > p3
+queue.prioritize(p1, 100);
+
+queue.print();
+
+assert.deepEqual(queue.peekPatient(), p1);
+queue.popPatient();
+assert.deepEqual(queue.peekPatient(), p2);
+queue.popPatient();
+assert.deepEqual(queue.peekPatient(), p3);
+
+queue.print();
